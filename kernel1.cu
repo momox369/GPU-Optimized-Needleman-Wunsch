77ae99533115230d18d74325e51d38705cd6a398
momox369 <salamehmaurice13@gmail.com>
2024-04-16 22:29:59 +0300

    kernel1 impl

--- kernel1.cu
+++ kernel1.cu
@@ -4,69 +4,50 @@
 #include "common.h"
 #include "timer.h"
 
-__shared__ int* currentDiagonal;
-__shared__ int* previousDiagonal;
-__shared__ int* previousPreviousDiagonal;
+__shared__ int* currentDiagonal[SEQUENCE_LENGTH];
+__shared__ int* previousDiagonal[SEQUENCE_LENGTH];
+__shared__ int* previousPreviousDiagonal[SEQUENCE_LENGTH];
 
 __global__ void kernel_nw1(unsigned char* sequence1, unsigned char* sequence2, int* scores_d, unsigned int numSequences)
 {
-    int matrixDim = SEQUENCE_LENGTH + 1;
-
-    currentDiagonal = (int*)malloc(SEQUENCE_LENGTH * sizeof(int));
-    previousDiagonal = (int*)malloc(SEQUENCE_LENGTH * sizeof(int));
-    previousPreviousDiagonal = (int*)malloc(SEQUENCE_LENGTH * sizeof(int));
-
     if(threadIdx.x == 0){
         //Initialize previous Diagonal from left to right bottom to top
         previousPreviousDiagonal[0] = 0;
-        previousDiagonal[0] = INSERTION;
-        previousDiagonal[1] = DELETION;
-    }
-
-    // 2 - Compute the scores for the rest of the matrix
-    int threadIteration = 0;
-
-    if(threadIdx.x == 0){
-        threadIteration = 2;
-    }
-
-    if(threadIdx.x == 1){
-        threadIteration = 1;
     }
     __syncthreads();
 
-    for (int diagIndex = 2; diagIndex < (2 * SEQUENCE_LENGTH) - 1 ; ++diagIndex) {
-        // Compute current diagonal from left to right bottom to top
-        int col = threadIdx.x;
-        int row = threadIteration; // the row being addressed, starts at 2
+    if (threadIdx.x == 1){
+        previousDiagonal[0] = INSERTION;
+        previousDiagonal[1] = DELETION;
+    }
+    __syncthreads();
 
-        if(threadIdx.x == 0){
-            currentDiagonal[0] = threadIteration * INSERTION;
-        }
+    // 2 - Compute the scores for the first part of the matrix
+    if (threadIdx.x >= 2 && threadIdx.x < SEQUENCE_LENGTH){
+        //initialize borders of the diagonal
+        currentDiagonal[0] = threadIdx.x * INSERTION;
+        currentDiagonal[threadIdx.x] = threadIdx.x * DELETION;
+        
+        //Compute internal elements
+        for (unsigned int i = 1; i < threadIdx.x; ++i){
+            unsigned int row = threadIdx.x - i + 1;
+            unsigned int col = i;
 
-        else if(row == 0){
-            currentDiagonal[col] = (threadIdx.x + 1) * DELETION;
-        }
+            int top = previousDiagonal[col];
+            int left = previousDiagonal[col - 1];
+            int topleft = previousPreviousDiagonal[col - 1];
 
-        else {
-            
-            if(threadIdx.x <= diagIndex && row < matrixDim && col < matrixDim) {
-            ++threadIteration;
-            int top     = (previousDiagonal[col]); //else, take the value directly above it
-            int left    = (previousDiagonal[col-1]); //else, take the value directly to the left of it
-            int topleft = (previousPreviousDiagonal[col-1]); //if not 1st row and not 1st col, take the value diagonally above and to the left
             int insertion = top + INSERTION;
             int deletion  = left + DELETION;
             int match     = topleft + (
-                (sequence2[blockIdx.x*SEQUENCE_LENGTH + (row - 1)] == sequence1[blockIdx.x*SEQUENCE_LENGTH + (col - 1)])
+                (sequence2[blockIdx.x*SEQUENCE_LENGTH + row] == sequence1[blockIdx.x*SEQUENCE_LENGTH + col])
                 ? MATCH 
                 : MISMATCH
                 ); //check if there is a match
             int max = (insertion > deletion) ? insertion : deletion; 
             max = (match > max) ? match : max;
-            currentDiagonal[col] = max; //store it in the matrix
 
-            }
+            currentDiagonal[col] = max; //store it in the matrix
         }
         __syncthreads();
 
@@ -76,6 +57,38 @@ __global__ void kernel_nw1(unsigned char* sequence1, unsigned char* sequence2, i
         previousDiagonal = currentDiagonal;
     }
     __syncthreads();
+
+    // 3 - Compute the scores for the second part of the matrix
+    if (threadIdx.x < SEQUENCE_LENGTH - 2){
+        //No need to initialize the borders of the diagonals in the second part
+        //Compute internal elements
+        for (unsigned int i = 1; i < SEQUENCE_LENGTH - threadIdx.x - 1; ++i){
+            unsigned int row = SEQUENCE_LENGTH - i;
+            unsigned int col = threadIdx.x + i;
+
+            int top = previousDiagonal[col];
+            int left = previousDiagonal[col - 1];
+            int topleft = previousPreviousDiagonal[col - 1];
+
+            int insertion = top + INSERTION;
+            int deletion  = left + DELETION;
+            int match     = topleft + (
+                (sequence2[blockIdx.x*SEQUENCE_LENGTH + row] == sequence1[blockIdx.x*SEQUENCE_LENGTH + col])
+                ? MATCH 
+                : MISMATCH
+                ); //check if there is a match
+            int max = (insertion > deletion) ? insertion : deletion; 
+            max = (match > max) ? match : max;
+
+            currentDiagonal[col] = max; //store it in the matrix
+        }
+        __syncthreads();
+
+        // Swap the buffers
+        int* temp = previousPreviousDiagonal;
+        previousPreviousDiagonal = previousDiagonal;
+        previousDiagonal = currentDiagonal;
+    }
 
     // 3 - Write the final score to the output array
     scores_d[blockIdx.x] = currentDiagonal[SEQUENCE_LENGTH - 1];
